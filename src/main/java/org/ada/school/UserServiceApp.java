2242ac534eb8ff9efafc82515ecbb4eac440b67c
Andres Calderon <andres.calderon@payu.com>
2021-08-28 10:58:26 -0500

    File formatting using the IntelliJ formatter tool.

--- src/main/java/org/ada/school/UserServiceApp.java
+++ src/main/java/org/ada/school/UserServiceApp.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApp
-{
+public class UserServiceApp {
 
-    public static void main( String[] args )
-    {
-        SpringApplication.run( UserServiceApp.class, args );
-    }
+	public static void main(String[] args) {
+
+		SpringApplication.run(UserServiceApp.class, args);
+	}
 
 }
