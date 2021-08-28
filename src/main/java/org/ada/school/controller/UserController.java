1034c1f327f5f45f8f3b65ab92a5efe1cca5bc39
Andres Calderon <andres.calderon@payu.com>
2021-08-28 11:04:56 -0500

    Rearrange code using Lombok dependency and Formatting code using the IntelliJ formatting tool.

--- src/main/java/org/ada/school/controller/UserController.java
+++ src/main/java/org/ada/school/controller/UserController.java
@@ -1,5 +1,8 @@
 package org.ada.school.controller;
 
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import org.ada.school.dto.UserDto;
 import org.ada.school.model.User;
 import org.ada.school.service.UserService;
@@ -14,50 +17,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping( "/user" )
-public class UserController
-{
+@RequestMapping("/user")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class UserController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    public UserController( @Autowired UserService userService )
-    {
-        this.userService = userService;
-    }
+	@GetMapping
+	public ResponseEntity<List<User>> all() {
 
+		return ResponseEntity.ok(userService.all());
+	}
 
-    @GetMapping
-    public ResponseEntity<List<User>> all()
-    {
-        return ResponseEntity.ok( userService.all() );
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable String id) {
 
-    @GetMapping( "/{id}" )
-    public ResponseEntity<User> findById( @PathVariable String id )
-    {
-        return ResponseEntity.ok( userService.findById( id ) );
-    }
+		return ResponseEntity.ok(userService.findById(id));
+	}
 
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody UserDto userDto) {
 
-    @PostMapping
-    public ResponseEntity<User> create( @RequestBody UserDto userDto )
-    {
-        return ResponseEntity.ok( userService.create( new User( userDto ) ) );
-    }
+		return ResponseEntity.ok(userService.create(new User(userDto)));
+	}
 
-    @PutMapping( "/{id}" )
-    public ResponseEntity<User> update( @RequestBody UserDto userDto, @PathVariable String id )
-    {
-        return ResponseEntity.ok( userService.update( userDto, id ) );
-    }
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@RequestBody UserDto userDto, @PathVariable String id) {
 
-    @DeleteMapping( "/{id}" )
-    public ResponseEntity<Boolean> delete( @PathVariable String id )
-    {
-        return ResponseEntity.ok( userService.deleteById( id ) );
-    }
+		return ResponseEntity.ok(userService.update(userDto, id));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> delete(@PathVariable String id) {
+
+		return ResponseEntity.ok(userService.deleteById(id));
+	}
 
 }
