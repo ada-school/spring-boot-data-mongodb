516b118c5405ece86c3aee83671df4bf57eafddf
Andres Calderon <andres.calderon@payu.com>
2021-08-29 13:18:12 -0500

    Addition of new methods and the @Service annotation for the UserService, also modifications of the methods that throws an UserServiceException.

--- src/main/java/org/ada/school/service/UserService.java
+++ src/main/java/org/ada/school/service/UserService.java
@@ -1,19 +1,27 @@
 package org.ada.school.service;
 
+import java.util.Date;
 import java.util.List;
 
 import org.ada.school.dto.UserDto;
+import org.ada.school.exception.UserServiceException;
 import org.ada.school.model.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
 
 	User create(User user);
 
-	User findById(String id);
+	User findById(String id) throws UserServiceException;
 
 	List<User> all();
 
 	boolean deleteById(String id);
 
-	User update(UserDto userDto, String id);
+	User update(UserDto userDto, String id) throws UserServiceException;
+
+	List<User> findUsersWithNameOrLastNameLike(String queryText);
+
+	List<User> findUsersCreatedAfter(Date startDate);
 }
