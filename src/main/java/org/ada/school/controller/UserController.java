4c8332387e6d394b4f66ed57fab39afcb410aa2a
Andres Calderon <andres.calderon@payu.com>
2021-08-29 13:13:23 -0500

    Addition of new used imports, RequestMapping is now set to the v1 version, Creation of the constructor using the @Qualifier annotation in order to make simple and understandable what service is the controller using at the moment, also the methods are now surrounded by a try catch in order to send more specific code messages when an exception appears this exception are now logged using Sl4j, and finally new methods added for new functionality as find by name or lastname and find the users that have been created after an specific date.

--- src/main/java/org/ada/school/controller/UserController.java
+++ src/main/java/org/ada/school/controller/UserController.java
@@ -1,12 +1,16 @@
 package org.ada.school.controller;
 
+import java.util.Date;
 import java.util.List;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.ada.school.dto.UserDto;
 import org.ada.school.model.User;
 import org.ada.school.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,41 +21,95 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequestMapping("/user")
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequestMapping("/v1/user")
 public class UserController {
 
 	private final UserService userService;
 
+	@Autowired
+	public UserController(@Qualifier("userServiceMongoDB") UserService userService) {
+
+		this.userService = userService;
+	}
+
 	@GetMapping
 	public ResponseEntity<List<User>> all() {
 
-		return ResponseEntity.ok(userService.all());
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(userService.all());
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<User> findById(@PathVariable String id) {
 
-		return ResponseEntity.ok(userService.findById(id));
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
 	}
 
 	@PostMapping
 	public ResponseEntity<User> create(@RequestBody UserDto userDto) {
 
-		return ResponseEntity.ok(userService.create(new User(userDto)));
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(new User(userDto)));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 	}
 
 	@PutMapping("/{id}")
 	public ResponseEntity<User> update(@RequestBody UserDto userDto, @PathVariable String id) {
 
-		return ResponseEntity.ok(userService.update(userDto, id));
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(userService.update(userDto, id));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Boolean> delete(@PathVariable String id) {
 
-		return ResponseEntity.ok(userService.deleteById(id));
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(userService.deleteById(id));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	@GetMapping("/NameOrLastname/{nameOrLastname}")
+	public ResponseEntity<List<User>> findUsersWithNameOrLastNameLike(@PathVariable String nameOrLastname) {
+
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(userService.findUsersWithNameOrLastNameLike(nameOrLastname));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
+
+	@GetMapping("/createdAfter/{createdAfter}")
+	public ResponseEntity<List<User>> findUsersWithNameOrLastNameLike(
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date createdAfter) {
+
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(userService.findUsersCreatedAfter(createdAfter));
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
 	}
 
 }
